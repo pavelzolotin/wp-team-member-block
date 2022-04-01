fa2b8bcddc5db1557ffce81dca02569dbd8a6cdf
pavelzolotin <pavel.zolotin@goat.digital>
2022-04-01 22:51:55 +0500

    create drug'n'drop social icons

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { registerBlockType } from '@wordpress/blocks';
+import './team-member';
+import './style.scss';
+import Edit from './edit';
+import save from './save';
+
+registerBlockType('block-course/team-members', {
+	edit: Edit,
+	save,
+});
